7395d7079da35507afda624fcd942eee4fd26ae3
Jason <jason.blank@outlook.com>
2019-12-20 04:57:49 +0100

    Add initial bluetooth-compatibility

--- robot10/robot10.ino
+++ robot10/robot10.ino
@@ -5,6 +5,23 @@
 
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
+#include <SoftwareSerial.h>
+
+
+// USB-config
+//ros::NodeHandle  nh;
+
+// Start Bluetooth-config
+// Comment below if needed to switch to USB-connection
+// If uncommented, comment USB-config
+
+class NewHardware : public ArduinoHardware {
+  public: NewHardware():ArduinoHardware(&Serial1, 57600){};
+}; 
+ros::NodeHandle_<NewHardware> nh;
+
+// End Bluetooth-config
+
 
 int forwardLeft = 2;
 int forwardRight = 6;
@@ -13,7 +30,6 @@ int reverseRight = 3;
 int enableLeft = 25;
 int enableRight = 24;
 
-ros::NodeHandle  nh;
 
 void messageCb( const geometry_msgs::Twist& msg){
   if(msg.linear.x > 0) {
@@ -34,7 +50,7 @@ void messageCb( const geometry_msgs::Twist& msg){
   }
 }
 
-ros::Subscriber<geometr2_msgs::Twist> sub("cmd_vel", &messageCb );
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
 
 void setup()
 { 
