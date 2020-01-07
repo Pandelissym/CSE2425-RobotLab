d2c9082fe0b6299e2f909bb2348f5cc82ab97b36
Pandelis Symeonidis <pandelissym@outlook.com>
2020-01-07 11:51:26 +0100

    Stop robot when object in front

--- robot10/robot10.ino
+++ robot10/robot10.ino
@@ -30,6 +30,8 @@ int reverseRight = 3;
 int enableLeft = 25;
 int enableRight = 24;
 
+int triggerPin = 23;
+int echoPin = 22;
 
 void messageCb( const geometry_msgs::Twist& msg){
   if(msg.linear.x > 0) {
@@ -69,5 +71,22 @@ void loop()
 {  
   nh.spinOnce();
   delay(1);
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+  distance= duration*0.034/2;
+  
+  if (distance >= 5) {
+    digitalWrite(enableLeft, LOW);
+    digitalWrite(enableRight, LOW);
+  }
+    
 }
 
