ab2946face728f676c8df42bf43eab5cc9c326da
Pandelis Symeonidis <pandelissym@outlook.com>
2020-01-20 11:25:21 +0100

    add linetracker

--- robot10/robot10.ino
+++ robot10/robot10.ino
@@ -6,19 +6,19 @@
 
 
 // USB-config
-ros::NodeHandle  nh;
+//ros::NodeHandle  nh;
 
 
 //// Start Bluetooth-config
 //// Comment below if needed to switch to USB-connection
 //// If uncommented, comment USB-config
 
-//class NewHardware : public ArduinoHardware {
-//  public: NewHardware():ArduinoHardware(&Serial1, 57600){};
-//}; 
-//ros::NodeHandle_<NewHardware> nh;
+class NewHardware : public ArduinoHardware {
+  public: NewHardware():ArduinoHardware(&Serial1, 57600){};
+}; 
+ros::NodeHandle_<NewHardware> nh;
 
-//// End Bluetooth-config
+//End Bluetooth-config
 
 
 int forwardLeft = 6;
@@ -34,51 +34,30 @@ boolean stop_moving = false;
 boolean isFirst = true;
 int distance = 0;
 
-
-// HCSR-04 implementation
-
-double measureDistanceCm(int triggerPin, int echoPin) {
-    // Make sure that trigger pin is LOW.
-    digitalWrite(triggerPin, LOW);
-    delayMicroseconds(2);
-    // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
-    digitalWrite(triggerPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(triggerPin, LOW);
-    // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
-    unsigned long durationMicroSec = pulseIn(echoPin, HIGH);
-    double distanceCm = durationMicroSec / 2.0 * 0.0343;
-    if (distanceCm == 0 || distanceCm > 400) {
-        return -1.0 ;
-    } else {
-        return distanceCm;
-    }
-}
-
 void goForward() {
   analogWrite(reverseLeft, 0);
   analogWrite(reverseRight, 0);
-  analogWrite(forwardLeft, 255);
-  analogWrite(forwardRight, 255);
+  analogWrite(forwardLeft, 80);
+  analogWrite(forwardRight, 80);
 }
 
 void goBackward() {
   analogWrite(forwardLeft, 0);
   analogWrite(forwardRight, 0);
-  analogWrite(reverseLeft, 255);
-  analogWrite(reverseRight, 255);
+  analogWrite(reverseLeft, 80);
+  analogWrite(reverseRight, 80);
 }
 
 void goLeft() {
-  analogWrite(forwardLeft, 62);
-  analogWrite(forwardRight, 255);
+  analogWrite(forwardLeft, 30);
+  analogWrite(forwardRight, 80);
   analogWrite(reverseLeft, 0);
   analogWrite(reverseRight, 0);
 }
 
 void goRight() {
-  analogWrite(forwardLeft, 255);
-  analogWrite(forwardRight, 62);
+  analogWrite(forwardLeft, 80);
+  analogWrite(forwardRight, 30);
   analogWrite(reverseLeft, 0);
   analogWrite(reverseRight, 0);  
 }
@@ -93,7 +72,7 @@ void stopMoving() {
 void messageCb( const geometry_msgs::Twist& msg) {
   resetTimer1();
     
-  if(msg.linear.x > 0) {
+  if(msg.linear.x > 0 && msg.angular.z == 0) {
     goForward();
   } else if(msg.linear.x < 0) {
     goBackward();
@@ -118,6 +97,8 @@ void setup() {
   pinMode(enableLeft, OUTPUT);
   pinMode(enableRight, OUTPUT);
   pinMode(13, OUTPUT);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   
   nh.initNode();
   nh.subscribe(sub);
@@ -135,17 +116,22 @@ void setup() {
 void loop() {    
   nh.spinOnce();
   
-  
-  distance = measureDistanceCm(triggerPin, echoPin);
-  if(distance < 5) {
+  digitalWrite(triggerPin, HIGH);
+  digitalWrite(triggerPin, LOW);
+ 
+  distance = pulseIn(echoPin, HIGH);
+  distance = distance / 2.0 * 0.0343;
+   if(distance < 10) {
       stopMoving();
-  }
+    }
+  
+    
 }
 
 ISR(timer1Event){
   stopMoving();
   
-  digitalWrite(13, HIGH);
+  //digitalWrite(13, HIGH);
   resetTimer1(); //reset timer1
 }
 
